035cbae178a5845120f6274cdc68264be040d2ca
camelys624 <camel_yangz@163.com>
2018-09-13 03:00:40 -0700

    learVue day1

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+const path = require('path');
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'js/[name].js'
+    }
+};
